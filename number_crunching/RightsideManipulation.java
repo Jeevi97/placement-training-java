0115604973000caf1bfd9c905908c03b2e17b01e
Jeevika E <160655146+Jeevi97@users.noreply.github.com>
2025-08-28 21:58:43 +0530

    Create RightsideManipulation.java

---
+++ number_crunching/RightsideManipulation.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class RightsideManipulation {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        long ld;
+        while(n>0){
+            ld=n%10;
+            n=n/10;
+            System.out.print(ld+" ");
+        }
+    }
+}
