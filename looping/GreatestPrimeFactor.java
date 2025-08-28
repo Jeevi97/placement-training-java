2d39144b1ae2460151c934e12fc68493db606ac3
Jeevika E <160655146+Jeevi97@users.noreply.github.com>
2025-08-28 22:26:43 +0530

    Update GreatestPrimeFactor.java

--- looping/GreatestPrimeFactor.java
+++ looping/GreatestPrimeFactor.java
@@ -7,7 +7,6 @@ import java.util.regex.*;
 public class GreatestPrimeFactor{
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int max=0;
