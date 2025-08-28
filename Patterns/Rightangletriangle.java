ced4826e072692195d39af295a1ea8664884aac4
Jeevika E <160655146+Jeevi97@users.noreply.github.com>
2025-08-28 22:06:12 +0530

    Create Rightangletriangle.java

---
+++ Patterns/Rightangletriangle.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Rightangletriangle{
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int rows=1;rows<=n;rows++){
+            for(int col=1;col<=rows;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
