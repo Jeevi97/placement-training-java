af6625a2cbcf5ef055e6383edf042f3b0942c483
Jeevika E <160655146+Jeevi97@users.noreply.github.com>
2025-08-28 22:15:30 +0530

    Create HallowSquarewithDiagonal.java

---
+++ Patterns/HallowSquarewithDiagonal.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class HallowSquarewithDiagonal{
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int size=2*n-1;
+        for(int rows=1;rows<=size;rows++){
+            for(int col=1;col<=size;col++){
+                if(rows==1||rows==size||col==1||col==size||rows==col||rows+col==size+1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
