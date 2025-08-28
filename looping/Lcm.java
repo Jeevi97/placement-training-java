63cb9129dbc25e8331d498d7b8707a29494bfdea
Jeevika E <160655146+Jeevi97@users.noreply.github.com>
2025-08-28 21:46:18 +0530

    Create Lcm.java

---
+++ looping/Lcm.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Lcm{
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int i=2;
+        while(true){
+        if(i%a==0 && i%b==0){
+            System.out.print(i);
+            break;
+        }
+            i++;
+}
+    }
+}
