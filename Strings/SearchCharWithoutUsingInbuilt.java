3a2210b032d7fdfddba3636322523e775f598a3a
Jeevika E <160655146+Jeevi97@users.noreply.github.com>
2025-08-29 07:37:16 +0530

    Create SearchCharWithoutUsingInbuilt.java

---
+++ Strings/SearchCharWithoutUsingInbuilt.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        char a=sc.next().charAt(0);
+        sc.nextLine();
+        String word=sc.nextLine();
+        if(word.contains(String.valueOf(a))){
+            System.out.print("Present");
+        }
+        else{
+            System.out.print("Not Present");
+        }
+    }
+}
