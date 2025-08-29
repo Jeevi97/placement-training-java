5d90a52822d4ea231abbd324a52f9779b893289b
Jeevika E <160655146+Jeevi97@users.noreply.github.com>
2025-08-29 07:08:17 +0530

    Update replacewithsumofeveryotherelement.java

--- Arrays/replacewithsumofeveryotherelement.java
+++ Arrays/replacewithsumofeveryotherelement.java
@@ -7,7 +7,6 @@ import java.util.regex.*;
 public class replacewithsumofeveryotherelement {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int sum=0,result;
