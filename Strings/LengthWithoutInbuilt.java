3c7cccde671a5470fa375b70506f06402e114803
Jeevika E <160655146+Jeevi97@users.noreply.github.com>
2025-08-29 07:15:06 +0530

    Create LengthWithoutInbuilt.java

---
+++ Strings/LengthWithoutInbuilt.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class LengthWithoutInbuilt {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            count++;
+    }
+    System.out.print(count);
+    }
+}
